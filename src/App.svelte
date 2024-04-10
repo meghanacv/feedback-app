e22833d78735b4d575f8e93c0325126d84589de5
Meghana C V <meghana.cvmcvm@gmail.com>
2024-04-10 22:14:02 +0530

    adding complete project

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,11 @@
 <script>
-  import FeedbackList from "./components/FeedbackList.svelte";
-
-  let feedback = [
-    {
-      id: "1",
-      rating: 10,
-      text: "Lorem ipsum dolor sit. amet consectetur, adipisicing elit Architecto",
-    },
-    {
-      id: "2",
-      rating: 8,
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in fuga veritatis aliquam sunt velit"
-    },
-    {
-      id: "3",
-      rating: 7,
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    }
-  ];
-
-  const deleteFeedback = (e) => {
-	const itemId = e.detail
-	feedback = feedback.filter((item) => item.id !== itemId)
-  }
+	import FeedbackForm from './components/FeedbackForm.svelte'
+	import FeedbackList from './components/FeedbackList.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
 </script>
 
 <main class="container">
-  <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
-  
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
 </main>
--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,11 @@
 <script>
-    import FeedbackItem from "./FeedbackItem.svelte";
-  export let feedback = [];
+  import { FeedbackStore } from "../stores";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
 </script>
 
-{#each feedback as fb (fb.id)}
-  <FeedbackItem item={fb} on:delete-feedback/>
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} />
+  </div>
 {/each}
